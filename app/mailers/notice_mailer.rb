6feca7638b4bc792c195722366ca42ed6d2cdcbc
kyungbae <kyungbae@gmail.com>
2012-04-14 21:54:16 -0400

    smtpsendgrid

--- app/mailers/notice_mailer.rb
+++ app/mailers/notice_mailer.rb
@@ -2,6 +2,6 @@ class NoticeMailer < ActionMailer::Base
 
   def notice_to_doc(study)
     @study = study
-    mail(:to => 'kyungbae@gmail', :subject => "You got a new DICOM file", :from => 'kyungbae@seededu.com')
+    mail(:to => 'kyungbae@gmail.com', :subject => "You got a new DICOM file", :from => 'kyungbae@seededu.com')
   end
 end
