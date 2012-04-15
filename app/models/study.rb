a7df240fcc0097763480c76b99ac70afaccad2ba
kyungbae <kyungbae@gmail.com>
2012-04-15 15:44:31 -0400

    email

--- app/models/study.rb
+++ app/models/study.rb
@@ -1,8 +1,12 @@
 class Study < ActiveRecord::Base
-  attr_accessible :name, :dicom_file, :status, :study_date
+  attr_accessible :name, :dicom_file, :status, :study_date, :email
   mount_uploader :dicom_file
   
   validates :dicom_file, :presence => true
+  validates :email, :presence => true, 
+    :format => { :with => /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i },
+    :message => 'Check the email address again.'
+    
   
   before_save :extract_data_from_dicom
   
@@ -13,7 +17,7 @@ class Study < ActiveRecord::Base
     if obj.read?
       
       self.name = obj.value("0010,0010")
-      #self.study_date = obj.value("0008,0020")
+      self.study_date = obj.value("0008,0020").to_s
       self.status = 'DICOM File'
     else
       self.name = 'N/A'
