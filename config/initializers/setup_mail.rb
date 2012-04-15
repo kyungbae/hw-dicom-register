6007a9be4bce39354b5b30172c69c4a14f9ef5f4
kyungbae <kyungbae@gmail.com>
2012-04-14 21:38:47 -0400

    fassmtp

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,9 +1,9 @@
 ActionMailer::Base.smtp_settings = {
-  :address              => "smtp.fas.harvard.edu",
-  :port                 => 25,
+  :address              => "smtp.sendgrid.net",
+  :port                 => 587,
   :domain               => "fas.harvard.edu",
-  :user_name            => "kbpark",
-  :password             => "gajigajigomH3r3",
-  :authentication       => "plain",
+  :user_name            => "app4006649@heroku.com",
+  :password             => "golgoru",
+  :authentication       => :login,
   :enable_starttls_auto => true
 }
